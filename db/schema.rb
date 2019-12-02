3400ca805269d6b8218e8327832fa2fd70690ad0
Pierre Jaouen <pierre.jaouen2014@gmail.com>
2019-12-02 14:25:24 -0500

    AddPhotoToSpots and AddReviewToSpots in the DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_202044) do
+ActiveRecord::Schema.define(version: 2019_12_02_183209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,8 @@ ActiveRecord::Schema.define(version: 2019_11_27_202044) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id"
+    t.string "review"
+    t.string "photo"
     t.index ["category_id"], name: "index_spots_on_category_id"
     t.index ["city_id"], name: "index_spots_on_city_id"
   end
